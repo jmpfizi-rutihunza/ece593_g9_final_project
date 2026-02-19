ff841bd72a4fa30ef3347282a2745c4f44201661
Saleh Esmaeil <110800015+SE72XI@users.noreply.github.com>
2026-02-18 23:45:16 -0800

    Update tb_top.sv

--- Multiprocessor_System_MS2_Class/CLASS_TB/tb_top.sv
+++ Multiprocessor_System_MS2_Class/CLASS_TB/tb_top.sv
@@ -1,3 +1,4 @@
+`include "environment.sv"
 `timescale 1ns/1ps
 
 module tb_top;
@@ -20,33 +21,38 @@ module tb_top;
   intf vif(clk);
 
   // connect reset + burst tag
-  assign vif.reset_n  = rst_n;
-  
+  //assign vif.reset_n  = rst_n;
+  assign vif.burst_id = 32'd0;   // driver can override if you want
 
   // REAL DUT instance
-  mp_dut #(.AW(11), .DW(8)) dut (
-    .clk      (clk),
-    .rst_n    (vif.reset_n),
+ mp_dut #(.AW(11), .DW(8)) dut (
+    .clk          (clk),
+    .rst_n        (vif.reset_n),
 
-    .core_id  (vif.core_id),
-    .opcode   (vif.opcode),
-    .req      (vif.req),
-    .gnt      (vif.gnt),
-    .we       (vif.we),
-    .addr     (vif.addr),
-    .data_in  (vif.data_in),
+    // Request signals
+    .core_id      (vif.core_id),
+    .opcode       (vif.opcode),
+    .req          (vif.req),
+    .addr         (vif.addr),
+    .A            (vif.A),      
+    .B            (vif.B),      
+    .we           (vif.we),
+    
+    .gnt          (vif.gnt),
 
-    .rvalid   (vif.rvalid),
-    .data_out (vif.data_out),
-
-    .burst_id (32'd0),
+    // Response signals
+    .rvalid       (vif.rvalid),
+    .data_out     (vif.data_out),
+    .core_id_out  (vif.core_id_out) // Added: essential for scoreboard
+    
+   
   );
 
   environment env;
 
   initial begin
     env = new(vif);
-    env.gen.tx_count = 20;   // required 15–20 transactions
+    env.gen.tx_count = 100;   
     env.run();
   end
 
