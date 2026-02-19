29cc503d144dea37b02f8edfb2e2b86e5589bf3e
Saleh Esmaeil <110800015+SE72XI@users.noreply.github.com>
2026-02-18 23:53:30 -0800

    Update tb_top.sv

--- Multiprocessor_System_MS2_Class/CLASS_TB/tb_top.sv
+++ Multiprocessor_System_MS2_Class/CLASS_TB/tb_top.sv
@@ -6,30 +6,30 @@ module tb_top;
   bit clk;
   bit rst_n;
 
-  // clock generation
+ 
   initial clk = 0;
   always #5 clk = ~clk;
 
-  // reset
+  
   initial begin
     rst_n = 0;
     #20;
     rst_n = 1;
   end
 
-  // interface instance
+  
   intf vif(clk);
 
-  // connect reset + burst tag
+  
   //assign vif.reset_n  = rst_n;
   assign vif.burst_id = 32'd0;   // driver can override if you want
 
-  // REAL DUT instance
+  
  mp_dut #(.AW(11), .DW(8)) dut (
     .clk          (clk),
     .rst_n        (vif.reset_n),
 
-    // Request signals
+    
     .core_id      (vif.core_id),
     .opcode       (vif.opcode),
     .req          (vif.req),
