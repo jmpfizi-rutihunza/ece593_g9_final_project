04d1150fa53538c3c8532873826eff69d023a2ff
Saleh Esmaeil <110800015+madebySal@users.noreply.github.com>
2026-02-25 23:37:33 -0800

    Create mp_pkg.sv

---
+++ Multiprocessor_System_MS4_UVM/UVM_TB/mp_pkg.sv
@@ -0,0 +1,15 @@
+package mp_pkg;
+
+  import uvm_pkg::*;
+  `include "uvm_macros.svh"
+
+  `include "mp_seq_item.sv"
+  `include "mp_driver.sv"
+  `include "mp_req_monitor.sv"
+  `include "mp_rsp_monitor.sv"
+  `include "mp_scoreboard.sv"
+  `include "mp_agent.sv"
+  `include "mp_env.sv"
+  `include "mp_test.sv"
+
+endpackage
