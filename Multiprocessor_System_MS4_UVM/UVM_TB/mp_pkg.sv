2c1399995f409f72c2d7399bb4f0ef309cd3f720
Saleh Esmaeil <110800015+madebySal@users.noreply.github.com>
2026-02-26 00:03:27 -0800

    Update mp_pkg.sv

--- Multiprocessor_System_MS4_UVM/UVM_TB/mp_pkg.sv
+++ Multiprocessor_System_MS4_UVM/UVM_TB/mp_pkg.sv
@@ -1,15 +1,18 @@
 package mp_pkg;
-
   import uvm_pkg::*;
   `include "uvm_macros.svh"
 
-  `include "mp_seq_item.sv"
-  `include "mp_driver.sv"
-  `include "mp_req_monitor.sv"
-  `include "mp_rsp_monitor.sv"
-  `include "mp_scoreboard.sv"
-  `include "mp_agent.sv"
-  `include "mp_env.sv"
-  `include "mp_test.sv"
+  `include "seq/mp_seq_item.sv"
+  `include "seq/mp_sequence.sv"
+
+  `include "agent/mp_driver.sv"
+  `include "agent/mp_req_monitor.sv"
+  `include "agent/mp_rsp_monitor.sv"
+  `include "agent/mp_agent.sv"
+
+  `include "env/mp_scoreboard.sv"
+  `include "env/mp_env.sv"
+
+  `include "test/mp_test.sv"
 
 endpackage
